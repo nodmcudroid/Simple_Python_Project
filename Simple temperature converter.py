b4dc1d2e5006043abaeb764414df0bb51b86f4b7
Prodipto Mondal Sonnet <191281189+nodmcudroid@users.noreply.github.com>
2024-12-12 00:48:49 +0600

    Update Simple temperature converter.py

--- Simple temperature converter.py
+++ Simple temperature converter.py
@@ -3,11 +3,11 @@ while True:
         unit = input("Enter (C or F):")
         num = float(input("Enter Number"))
         if unit == 'C':
-            d = num * 2
+            d = round((num * 9/5) +32 , 1)
             print(d)
             print("------------------------")
         elif unit == 'F':
-            d = num + 3
+            d = round((num -32)* 5/9 , 1)
             print(d)
             print("------------------------")
     except ValueError :
